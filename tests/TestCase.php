82de0e78b8f15e0dec7d22c484c370cb0558b35b
Steve McDougall <juststevemcd@gmail.com>
2023-06-06 18:28:22 +0100

    Setting up Pest

---
+++ tests/TestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustSteveKing\Launchpad\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    //
+}
