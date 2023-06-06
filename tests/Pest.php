f412aff4c694eaa5780076b32f39a9ae674823b5
Steve McDougall <juststevemcd@gmail.com>
2023-06-06 18:32:29 +0100

    Fixing tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,33 +2,6 @@
 
 declare(strict_types=1);
 
-uses(Tests\TestCase::class)->in('Feature');
+use JustSteveKing\Launchpad\Tests\TestCase;
 
-/*
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
-|
-*/
-
-expect()->extend('toBeOne', fn () => $this->toBe(1));
-
-/*
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
-|
-*/
-
-function something(): void
-{
-    // ..
-}
+uses(TestCase::class)->in('Feature');
